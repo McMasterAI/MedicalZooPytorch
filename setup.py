9fc74be425db69add1332d8585ffbd58a91ef200
unknown <shammo12345@hotmail.com>
2020-12-07 15:53:35 -0500

    Makes pip installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='RadiologyandAI-MedicalZooPytorch',
+    version='1.0',
+    packages=['RadiologyandAI-MedicalZooPytorch'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
+
